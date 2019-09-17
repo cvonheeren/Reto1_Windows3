8358431200c16ec23b0ecbe0332025da9b8ff4c2
IN1DM3B_15 <IN1DM3B_15@H015112.NET>
2019-09-17 09:56:45 +0200

    clase de formato estandar sin test

---
+++ src/main/java/Reto1_Windows3/leerFormatoEstandar.java
@@ -0,0 +1,35 @@
+package Reto1_Windows3;
+
+import java.util.Scanner;
+
+public class leerFormatoEstandar {
+
+	public boolean leerFormatoEstandar() {
+		
+	Scanner reader1 = new Scanner (System.in);
+	Scanner reader2 = new Scanner (System.in);
+	
+
+	
+	boolean igual = true;
+	
+
+	try {
+		String prueba1 = reader1.nextLine();
+		String prueba2 = reader2.nextLine();
+		
+		while(igual && prueba1 != null && prueba2 != null) {
+			if(!prueba1.equals(prueba2)) {
+				igual = false;
+			}
+				
+		}
+	} catch (Exception e) {
+		
+		e.printStackTrace();
+	} 
+
+	return false;
+}
+
+}
---
+++ src/test/java/Reto1_Windows3/leerFormatoEstandarTest.java
@@ -0,0 +1,18 @@
+package Reto1_Windows3;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class leerFormatoEstandarTest {
+
+	leerFormatoEstandar formatoestandar = new leerFormatoEstandar();
+	
+	@Test
+	public void leerFormatoEstandarTest() {
+		
+		assertEquals(LeerFormatoEstandarTest, true);
+		
+	}
+
+}
