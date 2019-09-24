50722aa1a7acb4f0d63a76d7c6f129b914f8ea5f
IN1DM3B_01 <IN1DM3B_01@H015112.NET>
2019-09-24 08:23:25 +0200

    arreglados repositorios

--- src/test/java/Reto1_Windows3/LeerFormatoEstandarTest.java
+++ src/test/java/Reto1_Windows3/LeerFormatoEstandarTest.java
@@ -1,18 +1,29 @@
 package Reto1_Windows3;
 
 import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
 import org.junit.Test;
 
 
 public class LeerFormatoEstandarTest {
 
-	LeerFormatoEstandar formatoestandar = new LeerFormatoEstandar();
+	  LeerFormatoEstandar formatoestandar = new LeerFormatoEstandar();
 	
 	@Test
 	public void leerFormatoEstandarTest() {
-		
-		//assertEquals(LeerFormatoEstandarTest, true);
-		
-	}
+	   
+	   String input = "hola";
+	   InputStream in = new ByteArrayInputStream (input.getBytes());
+	   System.setIn(in);
+	   Scanner reader = new Scanner (System.in);
+	   String resultado = "";
+	   resultado = formatoestandar.leerFormatoEstandar();
+	   assertEquals("hola", resultado);
+	   
+	} 
+	
 
 }
