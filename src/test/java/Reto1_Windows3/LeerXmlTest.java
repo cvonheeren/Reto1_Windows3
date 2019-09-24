762b99106fe87db0cb86633174535349f213a648
IN1DM3B_15 <IN1DM3B_15@H015112.NET>
2019-09-24 08:28:17 +0200

    arreglado un problema de merge con leerFormatoEstandarTest

---
+++ src/test/java/Reto1_Windows3/LeerXmlTest.java
@@ -0,0 +1,14 @@
+package Reto1_Windows3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LeerXmlTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
