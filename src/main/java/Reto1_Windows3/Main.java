71621e90a89ce890e1fc31bb9978d8f850675c74
IN1DM3B_15 <IN1DM3B_15@H015112.NET>
2019-09-30 12:49:36 +0200

    menu en el main

--- src/main/java/Reto1_Windows3/Main.java
+++ src/main/java/Reto1_Windows3/Main.java
@@ -1,20 +1,74 @@
 package Reto1_Windows3;
 
 import java.io.File;
+import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		JFileChooser chooser= new JFileChooser();
-		chooser.showDialog(null, "Abrir");
-		File archivo = chooser.getSelectedFile();
-		String archivoEntraStrg = archivo.getAbsolutePath();
 		
-		LeerTxt leerT = new LeerTxt();
-		System.out.println(leerT.leerText(archivoEntraStrg));
-	}
+		
+		JFileChooser chooser= new JFileChooser();
+		
+		
+		Scanner reader = new Scanner (System.in);
+		int tipo = 0;
+		
+		System.out.println("Eliga el tipo de archivo:");
+		System.out.println("1. Leer .txt\n2. Leer .xml\n3. Leer .csv\n4. Leer desde la entrada estandar");
+		System.out.print("Introduzca el numero: ");
+		tipo = reader.nextInt();
 
+			
+			if (tipo == 1) {
+					
+				
+				chooser.showDialog(null, "Abrir");
+				File archivo = chooser.getSelectedFile();
+				String archivoEntraStrg = archivo.getAbsolutePath();
+				
+				LeerTxt leerTxt = new LeerTxt();
+				System.out.println(leerTxt.leerText(archivoEntraStrg));
+				
+			
+			}
+			
+			if (tipo == 2) {
+				
+				
+				chooser.showDialog(null, "Abrir");
+				File archivo = chooser.getSelectedFile();
+				String archivoEntraStrg = archivo.getAbsolutePath();
+				
+				LeerFormatoXml leerXml = new LeerFormatoXml();
+				System.out.println(leerXml.leerXml(archivoEntraStrg));
+				
+				
+			}
+			
+			if (tipo == 3) {
+	
+				chooser.showDialog(null, "Abrir");
+				File archivo = chooser.getSelectedFile();
+				String archivoEntraStrg = archivo.getAbsolutePath();
+				
+				LeerFormatoCsv leerCsv = new LeerFormatoCsv();
+				System.out.println(leerCsv.leerCsv(archivoEntraStrg));
+				
+			}
+			
+			if (tipo == 4) {
+				
+				LeerFormatoEstandar leerFormatoEstandar = new LeerFormatoEstandar();
+				System.out.println(leerFormatoEstandar.leerFormatoEstandar());
+				
+			}
+		
+		
+		reader.close();
+		
+	
+	}
 }
